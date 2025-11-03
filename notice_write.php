bab97c850648f40f8348a37c3e7fde04bbcc2aff
hyejiining <hyejiining@gmail.com>
2025-11-03 22:02:06 +0900

    공지사항 페이지 수정

--- notice_write.php
+++ notice_write.php
@@ -85,41 +85,38 @@
 		<div id="fh5co-contact">
 			<div class="row animate-box">
 				<div class="col-md-6 col-md-offset-3 text-center fh5co-heading">
-					<h2>공지사항</h2>
+					<h2>Notice</h2>
+				</div>
+			</div>
+				<div class="col-md-6 animate-box">
+					<div class="row">
+						<div class="col-md-6">
+						<ul class="contact-info">
+							<li><i class="icon-profile"></i> Name</li>
+						</ul>
+						</div>
+						<div class="col-md-6">
+						<ul class="contact-info">
+							<li><i class="icon-calendar3"></i> 2025-11-02</li>
+						</ul>
+						</div>
+					</div>
+					</div>
+
+			<div class="col-md-12">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Title">
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="form-group">
+					<textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Content"></textarea>
 				</div>
 			</div>
 			<div class="row">
-				<div class="col-md-3 col-md-push-1 animate-box">
-					<h3>Our Address</h3>
-					<ul class="contact-info">
-						<li><i class="icon-location4"></i>198 West 21th Street, Suite 721 New York NY 10016</li>
-						<li><i class="icon-phone3"></i>+ 1235 2355 98</li>
-						<li><i class="icon-location3"></i><a href="#">info@yoursite.com</a></li>
-						<li><i class="icon-globe2"></i><a href="#">www.yoursite.com</a></li>
-					</ul>
-				</div>
-				<div class="col-md-7 col-md-push-1 animate-box">
-					<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Name">
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Email">
-							</div>
-						</div>
-						<div class="col-md-12">
-							<div class="form-group">
-								<textarea name="" class="form-control" id="" cols="30" rows="7" placeholder="Message"></textarea>
-							</div>
-						</div>
-						<div class="col-md-12">
-							<div class="form-group">
-								<input type="submit" value="Send Message" class="btn btn-primary btn-modify">
-							</div>
-						</div>
+				<div class="col-md-8 col-md-offset-2 animate-box">
+					<div class="form-group text-center">
+					<input type="submit" value="Send Message" class="btn btn-primary btn-modify">
 					</div>
 				</div>
 			</div>
@@ -154,7 +151,6 @@
 						<li><a href="#">About us</a></li>
 					</ul>
 				</div>
-
 				<div class="col-md-3">
 					<h4>Contact Information</h4>
 					<ul class="fh5co-footer-links">
